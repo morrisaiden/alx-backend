3b6868dbf3cc0cd3246906449e575b7c90c4785e
morrisaiden <mwaimorris1997@gmail.com>
2024-08-21 10:48:45 +0300

    Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+a class BasicCache that inherits from BaseCaching and is a caching system:
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """_summary_
+
+    Args:
+        BaseCaching (_type_): _description_
+    """
+    def put(self, key, item):
+        """_summary_
+
+        Args:
+            key (_type_): _description_
+            item (_type_): _description_
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+        
+    def get(self, key):
+        """_summary_
+
+        Args:
+            key (_type_): _description_
+        """
+        return self.cache_data.get(key, None)
+    
