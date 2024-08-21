98aba9bac5b6df8f3ffef0d51785fe694bf1808d
morrisaiden <mwaimorris1997@gmail.com>
2024-08-21 11:47:57 +0300

    FIFO caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,50 +1,35 @@
 #!/usr/bin/env python3
-"""_summary_
-
-    Returns:
-        _type_: _description_
-"""
-
-
 from collections import OrderedDict
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """_summary_
-
-    Args:
-        BaseCaching (_type_): _description_
-    """
+    """FIFO caching system implementation"""
 
     def __init__(self):
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """_summary_
-
-        Args:
-            key (_type_): _description_
-            item (_type_): _description_
-        """
-
+        """Add an item to the cache"""
         if key is None or item is None:
             return
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key, _ = self.cache_data.popitem(last=False)
-            print(f"DISCARD: {first_key}")
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+
+            oldest_key, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {oldest_key}")
 
         self.cache_data[key] = item
 
     def get(self, key):
-        """_summary_
-
-        Args:
-            key (_type_): _description_
-
-        Returns:
-            _type_: _description_
-        """
+        """Retrieve an item from the cache"""
+        if key is None:
+            return None
         return self.cache_data.get(key, None)
+
+    def print_cache(self):
+        """Print the current cache state"""
+        print("Current cache:")
+        for key, item in self.cache_data.items():
+            print(f"{key}: {item}")
